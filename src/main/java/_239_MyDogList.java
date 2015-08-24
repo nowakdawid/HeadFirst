17b5b2de7dc00396a2a44e77966be1b0208bd5b9
dnowak <dawid.nowak886@gmail.com>
2015-08-24 11:59:59 +0200

    created Dog class created _239_MyDogListTest class

--- src/main/java/_239_MyDogList.java
+++ src/main/java/_239_MyDogList.java
@@ -1,2 +1,16 @@
 public class _239_MyDogList {
+
+    private Dog[] dogs = new Dog[5];
+    private int i = 0;
+
+    public void addDog(Dog dog) {
+
+        if (i < dogs.length) {
+            dogs[i] = dog;
+            System.out.println("Dog added at " + i + " position!");
+            i++;
+        }
+
+    }
+
 }
---
+++ src/test/java/_239_MyDogListTest.java
@@ -0,0 +1,19 @@
+public class _239_MyDogListTest {
+
+    public static void main(String[] args) {
+
+        _239_MyDogList MyDogList = new _239_MyDogList();
+        Dog dog1 = new Dog();
+        MyDogList.addDog(dog1);
+        MyDogList.addDog(dog1);
+        MyDogList.addDog(dog1);
+        MyDogList.addDog(dog1);
+        MyDogList.addDog(dog1);
+        MyDogList.addDog(dog1);
+        MyDogList.addDog(dog1);
+        MyDogList.addDog(dog1);
+        MyDogList.addDog(dog1);
+
+    }
+
+}
