1e73be5f5b892cf9622e84a9d6ef60499ee4880f
dnowak <dawid.nowak886@gmail.com>
2015-09-18 21:16:19 +0200

    added _375, _379 and _382 packages and classes with Exceptions handling and music Midi funcionality

--- src/main/java/_351/MusicTest1.java
+++ src/main/java/_351/MusicTest1.java
@@ -6,33 +6,21 @@ import javax.sound.midi.Sequencer;
 
 public class MusicTest1 {
 
-    public void play() throws SomethingWrong {
-
-        if(true) { throw new SomethingWrong(); }
+    public void play() {
 
         try {
             Sequencer sequencer = MidiSystem.getSequencer();
-            System.out.println("Successfully got a sequencer");
         } catch (MidiUnavailableException e) {
-            e.getMessage();
+            System.out.println("Bummer");
         }
+        System.out.println("Successfully got a sequencer");
 
     }
 
     public static void main(String[] args) {
 
         MusicTest1 mt1 = new MusicTest1();
-        try {
-            mt1.play();
-            System.out.println("Lol!");
-        } catch (SomethingWrong somethingWrong) {
-            System.out.println("Yeah, something went wrong :/");
-            somethingWrong.printStackTrace();
-        }
-        finally {
-            System.out.println("But there is something more here!");
-        }
-        System.out.println("And there is more here!");
+        mt1.play();
 
     }
 
---
+++ src/main/java/_382/MyEx.java
@@ -0,0 +1,3 @@
+package _382;
+
+class MyEx extends Exception { }
