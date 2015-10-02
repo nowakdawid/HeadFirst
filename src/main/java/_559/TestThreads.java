4863a8e9f8bed8c007864b159583d565654b6df4
dnowak <dawid.nowak886@gmail.com>
2015-10-02 19:19:29 +0200

    added _559 packages and classes

---
+++ src/main/java/_559/TestThreads.java
@@ -0,0 +1,16 @@
+package _559;
+
+public class TestThreads {
+
+    public static void main(String[] args) {
+
+        ThreadOne t1 = new ThreadOne();
+        ThreadTwo t2 = new ThreadTwo();
+        Thread one = new Thread(t1);
+        Thread two = new Thread(t2);
+        one.start();
+        two.start();
+
+    }
+
+}
